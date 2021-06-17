8d4c4391e5d66b7ee40b60fd609176b8b137ada4
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-06-17 20:47:50 +0000

    Automatically backed up by Learn

--- src/CatComponent.js
+++ src/CatComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import {GraceHopperQuoteComponent} from "./CatComponent";
 
 export default class CatComponent extends Component {
   render() {
     return (
       <div className="bar" id="cat">
+        <GraceHopperQuoteComponent />
         <img src="/cat.gif" />
       </div>
     );
